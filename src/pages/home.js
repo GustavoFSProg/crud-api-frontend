6a0e11100b8a2a3497343b2e4cbf7f3db13b898e
Gustavo <gustavosohne38@gmail.com>
2020-06-21 18:38:55 -0300

    ender

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { useHistory } from 'react-router-dom'
 import Image from '../assets/imagem_1.jpeg'
 import api from '../services/api'
 import './style.css'
