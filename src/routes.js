17ca5ac6c4bb902711779779b7bb1c6ffbc19ead
Gustavo <gustavosohne38@gmail.com>
2020-06-22 10:39:45 -0300

    asdfasdf

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Home from './pages/home'
+// import Home from './pages/home'
 import listProducts from './pages/listProducts'
 
 export default function Routes() {
