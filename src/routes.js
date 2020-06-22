669d7470051913ddc9a24550b8ee7b1f266fd429
Gustavo <gustavosohne38@gmail.com>
2020-06-22 10:45:32 -0300

    dfdf

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-// import Home from './pages/home'
+import Home from './pages/home'
 import listProducts from './pages/listProducts'
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/" exact component={Home}></Route>
         <Route path="/list" component={listProducts}></Route>
       </Switch>
     </BrowserRouter>
