fe70886ca09becd8a9fa1f6fbec5f566a2bbbce6
Gustavo <gustavosohne38@gmail.com>
2020-06-22 23:00:02 -0300

    a\sdf

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,12 @@ import listProducts from './pages/listProducts'
 export default function Routes() {
   return (
     <>
-      <BrowserRouter >
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/list" component={listProducts} />
-      </Switch>
-      <BrowserRouter />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/list" component={listProducts} />
+        </Switch>
+      </BrowserRouter>
     </>
   )
 }
