4785ef26fda31c1cd5ba231df2a58b6518103cd6
Gustavo <gustavosohne38@gmail.com>
2020-06-23 14:52:42 -0300

    asf

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-// import Home from './pages/home'
+import Home from './pages/home'
 import listProducts from './pages/listProducts'
 
 function Routes() {
   return (
     <>
       <Switch>
-        {/* <Route exact path="/" component={Home} />*/}
-        <Route path="/" component={listProducts} />
+        <Route exact path="/" component={Home} />
+        <Route
+          path="https://crud-front.netlify.app/list"
+          component={listProducts}
+        />
       </Switch>
     </>
   )
