4acb51d5e6dee1461ef5c18c05b8aa161699de20
Gustavo <gustavosohne38@gmail.com>
2020-06-23 14:42:29 -0300

    asdf

--- src/services/api.js
+++ src/services/api.js
@@ -1,27 +1,8 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://crud-api-no.herokuapp.com/'
-// axios.defaults.baseURL = 'http://localhost:4000/'
-
-axios.interceptors.request.use(
-  (config) => config,
-  (error) => Promise.reject(error)
-)
-
-axios.interceptors.request.use(
-  (config) => {
-    return { ...config, headers: { 'Content-Type': 'application/json' } }
-  },
-
-  (error) => {
-    const { status } = error.response
-
-    if (status === 401) {
-    }
-    return Promise.reject(error)
-  }
-)
-
-const api = axios
+const api = axios.create({
+  baseURL: 'https://crud-api-no.herokuapp.com/',
+  // baseURL: 'http://localhost:4000',
+})
 
 export default api
